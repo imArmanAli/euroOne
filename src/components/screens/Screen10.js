482ce203cb01f1c7606399929a21ffe03f7f36d9
unknown <shabir.abdulmajeed786@gmail.com>
2021-05-11 05:55:22 +0500

    Add all my files

---
+++ src/components/screens/Screen10.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import Slider from "../../slider/Slider";
+function Screen2() {
+  const [clicked1, setClicked1] = useState(true);
+  const [clicked2, setClicked2] = useState(true);
+  return (
+    <div className="container mb-5 colored step10 d-flex flex-column  justify-content-center align-items-center">
+      <h4 style={{ fontWeight: 400 }}>
+        <strong>Wieviel Wohnfläche </strong> sollen versichert werden?
+      </h4>
+
+      <main className="row default_wrapper">
+      <div className="sl">
+              <Slider
+        
+                left_text="<50 m²"
+                right_text=">500 m²"
+                value="0"
+                min="0"
+                max="450"
+                step="150"
+                unit="m²"
+                text="Alternativ eintippen"
+              />
+            </div>
+        <Link style={{ margin: "0 10px", width: "100%" }} to="/Step4">
+          <button>
+            <i class="fas fa-angle-right" aria-hidden="true"></i> WEITER
+          </button>
+        </Link>
+        <p className="text-center">
+        <i className="fas fa-lock mr-1" aria-hidden="true"></i>
+        Ihre Daten werden vertraulich behandelt und nicht an Dritte
+        weitergegeben.
+      </p>
+      </main>
+    </div>
+  );
+}
+
+export default Screen2;
---
+++ src/components/screens/Screen6.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+function Default() {
+  const [clicked1, setClicked1] = useState(true);
+  const [clicked2, setClicked2] = useState(true);
+  const [clicked3, setClicked3] = useState(true);
+  const [clicked4, setClicked4] = useState(true);
+  return (
+    <div className="container mb-5 colored step6 d-flex flex-column  justify-content-center align-items-center">
+      <h4 style={{ fontWeight: 400 }}>
+        Welche Absicherungen <strong>wünschen</strong> Sie?
+        <br />
+        <small style={{ color: "#777", fontSize: "12px" }}>
+          (Mehrfach&shy;auswahl möglich)
+        </small>
+      </h4>
+
+      <main className="row default_wrapper">
+      <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked1(!clicked1)}
+          className={
+            clicked1
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Angestellte/r</h3>
+        </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+          <div
+            onClick={() => setClicked2(!clicked2)}
+            className={
+              clicked2
+                ? "col-xs-6 px-2 default_box"
+                : "col-xs-6 px-2 default_box theme_bg b"
+            }
+          >
+            <h3> Beamte/r</h3>
+          </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked3(!clicked3)}
+          className={
+            clicked3
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Selbstständige/r</h3>
+        </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked4(!clicked4)}
+          className={
+            clicked4
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Student / Schüler</h3>
+        </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked4(!clicked4)}
+          className={
+            clicked4
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Hausfrau/mann</h3>
+        </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked4(!clicked4)}
+          className={
+            clicked4
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Rentner/in</h3>
+        </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked4(!clicked4)}
+          className={
+            clicked4
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Arbeitssuchend</h3>
+        </div>
+        </Link>
+        <Link className="form_btn_big" to="/Step7">
+        <div
+          onClick={() => setClicked4(!clicked4)}
+          className={
+            clicked4
+              ? "col-xs-6 px-2 default_box"
+              : "col-xs-6 px-2 default_box theme_bg"
+          }
+        >
+          <h3> Sonstiges</h3>
+        </div>
+        </Link>
+        <Link style={{ margin: "0 10px", width: "100%" }} to="/Step2">
+          <button>
+            <i class="fas fa-angle-right" aria-hidden="true"></i> WEITER
+          </button>
+        </Link>
+      </main>
+    </div>
+  );
+}
+
+export default Default;
